06d09f92eee7809a0bdc4d742b035783b106659e
MartinIglic <martin.iglic@gmail.com>
2024-07-19 13:31:28 +0200

    Dodal sem še zadnjo tabelo, z vsemi komentarji in zunanjimi ključi. ČE mi jih bo baza sprejela, je potem baza postavljena in potrebuje le še dodajanje pravic.

--- baza 1.02.sql
+++ baza 1.02.sql
@@ -6,7 +6,6 @@ CREATE TABLE Portfelj (
 	);
 
 
-
 CREATE TABLE Uporabnik (
     ID SERIAL PRIMARY KEY,            -- Primarni ključ, samodejno se generira
     ime VARCHAR(50) NOT NULL,         -- Besedilni stolpec, omejen na 50 znakov, ne sme biti prazen
@@ -21,15 +20,21 @@ CREATE TABLE Vrednosti_od_papirjev(
     ID_Papirja INT NOT NULL,           -- Zunanji ključ od papirja, tam se prebere osnovne podatke o vrednostnem papirju
     Datum DATE DEFAULT CURRENT_DATE NOT NULL    -- Datum vnosa vrednosti 
     Vrednost NUMERIC(10,2) NOT NULL    -- Pove vrednost delnice vrednostnega papirja na datum vnosa
-    CONSTRAINT 
+    CONSTRAINT ID_Papirja_kratica FOREIGN KEY (ID_Papirja) REFERENCES Vrednostni_papirji(Kratica) -- Ključ v drugi tabeli, je naveden kot kratica, da so podatki bolj pregledni
 );
 
+
 CREATE TABLE Portfeljske_transakcije (
-	ID SERIAL PRIMARY KEY UNIQUE,			-- PRimarni ključ, se samodejno generira
-	ID_Portfelja INT NOT NULL,
-	
-	
+	ID SERIAL PRIMARY KEY UNIQUE,			-- PRimarni ključ, se samodejno generira, 
+	ID_Portfelja INT NOT NULL,              -- Pove, kateri portfelj je imel to transakcijo, zunanji ključ
+    ID_Papirja CHAR(4) NOT NULL,            -- S katerim papirjem trguje
+    Datum_transakcije DATE DEFAULT CURRENT_DATE NOT NULL, -- Datum transakcije
+    Količina NUMERIC(10,4) DEFAULT 1 NOT NULL,            -- Pove količino, sprejema tudi negativne, kar pomeni prodaja, pozitivna nakup
+	Opis VARCHAR(1000)                      -- Po želji doda opis, razlog nakupa, recimo hedging
+    CONSTRAINT ID_Portfelja FOREIGN KEY (ID_Portfelja) REFERENCES Portfelj(ID)  --  Navezuje se na kateri portfelj se nanaša transkacija
+    CONSTRAINT ID_Papirja FOREIGN KEY (ID_Papirja) REFERENCES Vrednosti_od_papirjev(ID) --Navezuje se na vrsto papirja 
 );
+
 
 CREATE TABLE Vrednostni_papirji (
     ID SERIAL PRIMARY KEY,              -- Primarni ključ, ki bo u bistvu zaporedna številka nekega vrednostnega papirja
